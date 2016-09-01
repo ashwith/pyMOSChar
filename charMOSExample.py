86c64ef845aa131543393b34c15791600e4ef8bc
Ashwith Jerome Rego <ashwith@gmail.com>
2016-09-01 14:30:21 +0530

    Modified to reflect multiple include files support

--- charMOSExample.py
+++ charMOSExample.py
@@ -3,5 +3,5 @@
 import charMOS
 import numpy as np
 mosLengths = np.concatenate((np.arange(0.18, 0.3, 0.02), np.arange(0.3, 2, 0.2), np.arange(2, 10, 0.5), np.array([10])))
-charMOS.init(mosLengths=mosLengths, modelFile='tsmc180.mod')
+charMOS.init(mosLengths=mosLengths, modelFiles=('tsmc180.mod',))
 charMOS.genDB()
